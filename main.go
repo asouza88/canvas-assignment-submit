635b70b35434bf122315caba7504c992b4e66d23
Anthony J Souza <ajsouza88@gmail.com>
2023-11-14 14:11:12 -0800

    Added a few options to upload script
    -- can specify header row index
    -- can specify student id col index
    -- can specify score col index
    -- can specify comment col index

--- main.go
+++ main.go
@@ -27,7 +27,21 @@ type ResponseResult struct {
 	code int
 }
 
-func readCSV(filename string) (map[string]string, error) {
+type AssignmentFeedback struct {
+	studentId string
+	score     string
+	comment   string
+}
+
+type ReaderOptions struct {
+	headerRow    int
+	scoreCol     int
+	commentCol   int
+	studentIdCol int
+}
+
+func readCSV(filename string, options ReaderOptions) (map[string]AssignmentFeedback, error) {
+
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("Error opening CSV file:", err)
@@ -35,11 +49,19 @@ func readCSV(filename string) (map[string]string, error) {
 	}
 
 	reader := csv.NewReader(file)
+	for i := 0; i < options.headerRow-1; i++ {
+		_, err := reader.Read()
+		if err != nil {
+			return nil, err
+		}
+	}
 	headerRow, err := reader.Read()
 	if err != nil {
 		return nil, err
-	} else {
-		log.Default().Println(headerRow)
+	}
+	sizeOfRow := len(headerRow)
+	if sizeOfRow < options.scoreCol || sizeOfRow < options.studentIdCol || sizeOfRow < options.commentCol {
+		return nil, fmt.Errorf("invalid index used for given reader options. Row length %d , Options: %v\n", sizeOfRow, options)
 	}
 
 	records, err := reader.ReadAll()
@@ -52,28 +74,32 @@ func readCSV(filename string) (map[string]string, error) {
 		fmt.Println("Error reading CSV:", err)
 		return nil, err
 	}
-	attendData := make(map[string]string)
+	attendData := make(map[string]AssignmentFeedback)
 
 	for _, record := range records {
-		attendData[record[0]] = record[1]
+		attendData[record[options.studentIdCol]] = AssignmentFeedback{
+			studentId: record[options.studentIdCol],
+			score:     record[options.scoreCol],
+			comment:   record[options.commentCol],
+		}
 	}
 
 	return attendData, nil
 }
 
-func buildRequests(data map[string]string, courseNumber int, assignmentNumber int) ([]CanvasRequest, error) {
+func buildRequests(data map[string]AssignmentFeedback, courseNumber int, assignmentNumber int) ([]CanvasRequest, error) {
 	reqTotal := len(data)
 	reqCnt := 0
 	reqs := make([]CanvasRequest, reqTotal)
 	baseDomain := os.Getenv("CANVAS_DOMAIN")
 	apikey := os.Getenv("CANVAS_API")
-	for studentId, magicWord := range data {
+	for studentId, feedbackInfo := range data {
 		reqs[reqCnt] = CanvasRequest{
 			url:    fmt.Sprintf("%s/courses/%d/assignments/%d/submissions/sis_user_id:%s", baseDomain, courseNumber, assignmentNumber, studentId),
 			method: "PUT",
 			data: map[string]string{
-				"comment[text_comment]":    magicWord,
-				"submission[posted_grade]": "1",
+				"comment[text_comment]":    feedbackInfo.comment,
+				"submission[posted_grade]": feedbackInfo.score,
 			},
 			dataType: "text",
 			headers: map[string]string{
@@ -148,12 +174,16 @@ func main() {
 	var assignID int
 	var csvFile string
 	var headerRow int
-	var scoreColumn int
+	var studentIdCol int
+	var scoreCol int
+	var commentCol int
 
 	rootCmd.Flags().IntVarP(&courseID, "courseid", "c", 0, "Course ID")
 	rootCmd.Flags().IntVarP(&assignID, "assignid", "a", 0, "Attendance Assignment ID")
-	rootCmd.Flags().IntVarP(&headerRow, "headerow", "r", 0, "Index of header row, default 0")
-	rootCmd.Flags().IntVarP(&scoreColumn, "scorecol", "s", 1, "Index of score column")
+	rootCmd.Flags().IntVarP(&headerRow, "headerow", "r", 0, "Index of header row (default 0)")
+	rootCmd.Flags().IntVarP(&studentIdCol, "sid", "i", 0, "Index of student id column (default 0)")
+	rootCmd.Flags().IntVarP(&scoreCol, "score", "s", 1, "Index of score column")
+	rootCmd.Flags().IntVarP(&commentCol, "comment", "t", 2, "Index of comment column")
 
 	rootCmd.Run = func(cmd *cobra.Command, args []string) {
 
@@ -165,14 +195,20 @@ func main() {
 			}
 			return
 		}
+		fmt.Printf("Course Id: %d\nAssign Id: %d\nHeader Row index: %d\nStudent Id Col: %d\nScore Col: %d\nComment Col: %d\nFile: %s\n", courseID, assignID, headerRow, studentIdCol, scoreCol, commentCol, csvFile)
 
-		fmt.Printf("Recording attendance for Course ID: %d, Attendance ID: %d, CSV File: %s\n", courseID, assignID, csvFile)
-		// Implement your logic to record attendance here.
-		data, err := readCSV(csvFile)
+		//Implement your logic to record attendance here.
+		data, err := readCSV(csvFile, ReaderOptions{
+			headerRow:    headerRow,
+			studentIdCol: studentIdCol,
+			scoreCol:     scoreCol,
+			commentCol:   commentCol,
+		})
 		if err != nil {
 			logger.Printf("Error Running command %v", err)
 			os.Exit(-2)
 		}
+
 		builtReqs, err := buildRequests(data, courseID, assignID)
 		if err != nil {
 			logger.Printf("Error Running command %v", err)
